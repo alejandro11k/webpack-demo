b357a5fdfad58fbe3d7734d5003c62232e4f83fa
alejandro11k <k11alejandro@gmail.com>
2017-09-18 10:05:17 -0300

    fist commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+//to run ./node_modules/.bin/webpack --config webpack.config.js
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
